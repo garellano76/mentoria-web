9b7490c532175520983a9ae2c7904ee19d241f0a
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-04-13 13:00:27 -0400

    Commit 13042021_4

--- code/db-basico.php
+++ code/db-basico.php
@@ -5,7 +5,7 @@ $dbuser = "registro-user";
 $dbpassword = "user1";
 
 try {
-    $dsn = "mysql;host=localhost;dbname=$dbname";
+    $dsn = "mysql:host=localhost;dbname=$dbname";
     $db = new PDO($dsn,$dbuser,$dbpassword);   
     echo "conexion correcta"; 
 } catch (PDOException $e){
