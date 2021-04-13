5e5e9b13f96cd2d4bfe8646b476ce2b57c4eb1d7
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-04-13 12:27:17 -0400

    Commit 13042021

--- code/db-basico.php
+++ code/db-basico.php
@@ -1,12 +1,12 @@
 <?php
 
-$dbname = "registro";
+$dbname = "registro44444";
 $dbuser = "registro-user";
 $dbpassword = "user1";
 
 try {
     $dsn = "mysql;host=localhost;dbname=$dbname";
-    $db = new PDO($dsn,$dbuser,$dbpassword);
+    $db = new PDO($dsn,$dbuser,$dbpassword);    
 } catch (PDOException $e){
     echo $e->getmessage();
 }
