7fae7d14d4024c4c1722e663871b7d7585eadfb4
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-04-13 15:20:18 -0400

    Commit 13042021_5

--- code/db-basico.php
+++ code/db-basico.php
@@ -12,4 +12,25 @@ try {
     echo $e->getmessage();
 }
 
+// Preparar consulta
+$sql = "INSERT INTO  users 
+            (full_name, email, user_name, password)
+          VALUES
+            (:full_name, :email, :user_name, :password)";
+
+//statement            
+$stmt = $db->prepare($sql);
+
+$full_name = 'Juan Perez';
+$email = 'juan.perez@segic.cl';
+$user_name = 'juan.perez';
+$password = 'juan123';
+
+$stmt->bindParam(':full_name', $full_name);
+$stmt->bindParam(':email', $email);
+$stmt->bindParam(':user_name', $user_name);
+$stmt->bindParam(':password', $password);
+
+$stmt->execute();
+
 
