1aedab9990b0925a893100b1b59d4be6c12f09b8
root <root@ADMINRG-FD4716S.localdomain>
2021-05-05 10:34:22 -0400

    Commit 05052021_01

--- code/excel.php
+++ code/excel.php
@@ -21,17 +21,25 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
-$sheet->setCellValue('A1', 'ID');
+/*$sheet->setCellValue('A1', 'ID');
 $sheet->setCellValue('B1', 'FULL_NAME');
 $sheet->setCellValue('C1', 'EMAIL');
-$sheet->setCellValue('D1', 'USER');
+$sheet->setCellValue('D1', 'USER');*/
+$sheet->setCellValueByColumnAndRow(1,1, 'ID');
+$sheet->setCellValueByColumnAndRow(2,1, 'FULL_NAME');
+$sheet->setCellValueByColumnAndRow(3,1, 'EMAIL');
+$sheet->setCellValueByColumnAndRow(4,1, 'USER');
 
 foreach($users as $key => $user) {
     $llave = $key + 2;
-    $sheet->setCellValue('A'.$llave, $user['id']);
+    /*$sheet->setCellValue('A'.$llave, $user['id']);
     $sheet->setCellValue('B'.$llave, $user['full_name']);
     $sheet->setCellValue('C'.$llave, $user['email']);
-    $sheet->setCellValue('D'.$llave, $user['user_name']);
+    $sheet->setCellValue('D'.$llave, $user['user_name']);*/
+    $sheet->setCellValueByColumnAndRow(1, $llave, $user['id']);
+    $sheet->setCellValueByColumnAndRow(1, $llave, $user['full_name']);
+    $sheet->setCellValueByColumnAndRow(1, $llave, $user['email']);
+    $sheet->setCellValueByColumnAndRow(1, $llave, $user['user_name']);
 }
 
 $writer = new Xlsx($spreadsheet);
