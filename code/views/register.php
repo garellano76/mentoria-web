292b483fa29b9eef4d3caa03424ea92b28ddaa43
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-06-01 10:18:36 -0400

    commit 01062021_01

---
+++ code/views/register.php
@@ -0,0 +1,26 @@
+<h1>Register</h1>
+
+<form method="POST">
+  <div class="mb-3">
+    <label class="form-label">Firstname</label>
+    <input type="text" name="firstname" class="form-control">    
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Lasttname</label>
+    <input type="text" name="lastname" class="form-control">    
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Email</label>
+    <input type="text" name="email" class="form-control">    
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Password</label>
+    <input type="password" name="password" class="form-control">    
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Confirm Password</label>    
+    <textarea type="password" name="confirmPassword" id="" cols="30" rows="10"></textarea>    
+  </div>
+
+  <button type="submit" class="btn btn-primary">Send</button>
+</form>
