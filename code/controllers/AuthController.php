fb2b1d0b74530317ebc8c407097c37c91c6591fc
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-06-03 16:23:21 -0400

    commit 03062021_01

--- code/controllers/AuthController.php
+++ code/controllers/AuthController.php
@@ -2,25 +2,34 @@
 
 namespace app\controllers;
 
-use  app\core\Request;
-use  app\core\Controller;
-use  app\models\RegisterModel;
+use app\core\Request;
+use app\core\Controller;
+use app\models\RegisterModel;
 
-class AuthController extends Controller
-{
-    public function login()
-    {
+class AuthController extends Controller{
+    public function login(){
         $this->setLayout('auth');
         return $this->render('login');
     }
 
-    public function register(Request $request)
-    {
+    public function register(Request $request){
         $this->setLayout('auth');
+
         if ($request->isPost()){
             $registerModel = new RegisterModel();
-            return "Procesando Datos del formulario";
-        }
+
+            $registerModel->loadData($request->getBody());
+
+            if ($registerModel->validate() && $registerModel->save()){
+                return 'Success';
+            }
+            
+            echo "<pre>";
+            var_dump($registerModel->errors);
+            echo "</pre>";
+
+            return 'Procesando datos del Formulario..';
+        } 
         return $this->render('register');
     }
 }
