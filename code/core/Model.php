33a17bced917d9c6edb8193437e50a892e4095eb
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-06-10 10:28:40 -0400

    commit 10062021_02

--- code/core/Model.php
+++ code/core/Model.php
@@ -36,26 +36,35 @@ abstract class Model{
                     $this->addError($attribute, self::RULE_REQUIRED);
                 }
 
-                if ($rulename === self::RULE_EMAIL && filter_var($value, FILTER_VALIDATE_EMAIL)){
+                if ($rulename === self::RULE_EMAIL && !filter_var($value, FILTER_VALIDATE_EMAIL)){
                     // agregar Error
                     $this->addError($attribute, self::RULE_EMAIL);
                 }
 
                 if ($rulename === self::RULE_MIN && strlen($value) < $rule['min']){
                     // agregar Error
-                    $this->addError($attribute, self::RULE_MIN);
+                    $this->addError($attribute, self::RULE_MIN, $rule);
                 }
 
                 if ($rulename === self::RULE_MAX && strlen($value) > $rule['max']){
                     // agregar Error
-                    $this->addError($attribute, self::RULE_MAX);
+                    $this->addError($attribute, self::RULE_MAX, $rule);
+                }
+                
+                if ($rulename === self::RULE_MATCH && $value != $this->{$rule['match']}){
+                    // agregar Error
+                    $this->addError($attribute, self::RULE_MATCH, $rule);
                 }
             }
         }
         return empty($this->errors);
     }
-    public function addError($attribute, $rule){
+    public function addError($attribute, $rule, $params = []){
         $message = $this->errorMessages()[$rule] ?? '';
+        
+        foreach($params as $key => $param){
+            $message = str_replace("{{$key}}", $param, $message);
+        }        
 
         $this->errors[$attribute][] = $message;
     }
@@ -66,7 +75,7 @@ abstract class Model{
             self::RULE_EMAIL => 'This field must be an email',
             self::RULE_MIN => 'Min length of the field must be {min}',
             self::RULE_MAX => 'Max length of the field must be {max}',
-            self::RULE_MATCH => 'This fiels must be the same as {attribute}',
+            self::RULE_MATCH => 'This fiels must be the same as {match}',
         ];
     }
 
