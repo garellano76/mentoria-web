d14e485681fb412815b80df60773fbac425b9a06
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-06-11 19:03:52 -0400

    commit 11062021_01

---
+++ code/core/widgets/Form.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\core\widgets;
+
+use app\core\Model;
+
+class Form
+{
+    public static function begin($action, $method)
+    {
+      //return sprintf('<form action="%s" method="%s">', $action, $method)
+      echo sprintf('<form action="%s" method="%s">', $action, $method);
+      return new Form();
+    }
+
+    public static function end()
+    {
+        return '</form';
+    }
+
+    public function field(Model $model, $attribute)
+    {
+        return new Field($model, $attribute);
+    }
+}
--- code/views/register.php
+++ code/views/register.php
@@ -1,38 +1,12 @@
-<h1>Register !!!</h1>
+<h1>Register</h1>
 
-<form method="POST">
-  <div class="mb-3">
-    <label class="form-label">FirstName</label>
-    <input type="text" name="firstName" value="<?= $model->firstName ?>" 
-    class="form-control <?= $model->hasError('firstName') ? 'is-invalid' : '' ?>">
-    <div class="invlaid-feedback">
-      <?= $model->getFirstError('firstName') ?>
-    </div>
-  </div>
-  <div class="mb-3">
-    <label class="form-label">LastName</label>
-    <input type="text" name="lastName" value="<?= $model->lastName ?>" 
-    class="form-control <?= $model->hasError('lastName') ? 'is-invalid' : '' ?>">
-    <div class="invlaid-feedback">
-      <?= $model->getFirstError('lastName') ?>
-    </div>
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Email</label>
-    <input type="text" name="email" value="<?= $model->email ?>" 
-    class="form-control <?= $model->hasError('email') ? 'is-invalid' : '' ?>">
-    <div class="invlaid-feedback">
-      <?= $model->getFirstError('email') ?>
-    </div>
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Password</label>
-    <input type="password" name="password" class="form-control">
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Confirm Password</label>
-    <input type="password" name="confirmPassword" class="form-control">
-  </div>
+<?php $form = \app\core\widgets\Form::begin('', 'POST') ?>
+   <?= $form->field($model, 'firstname') ?>
+   <?= $form->field($model, 'lastname') ?>
+   <?= $form->field($model, 'email') ?>
+   <?= $form->field($model, 'password') ?>
+   <?= $form->field($model, 'confirmPassword') ?>
 
-  <button type="submit" class="btn btn-primary">Save</button>
-</form>
+   <button type="submit" class="btn btn-primary">Save</button>
+<?php \app\core\widgets\Form::end() ?>
+
