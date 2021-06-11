a9b18161ee7da6d0b2e1163d0da8af4a2dbaec5a
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-06-11 19:28:13 -0400

    commit 11062021_06

--- code/views/register.php
+++ code/views/register.php
@@ -1,8 +1,8 @@
 <h1>Register</h1>
 
 <?php $form = \app\core\widgets\Form::begin('', 'POST') ?>
-   <?= $form->field($model, 'firstname') ?>
-   <?= $form->field($model, 'lastname') ?>
+   <?= $form->field($model, 'firstName') ?>
+   <?= $form->field($model, 'lastName') ?>
    <?= $form->field($model, 'email')->mailField() ?>
    <?= $form->field($model, 'password')->passwordField() ?>
    <?= $form->field($model, 'confirmPassword')->passwordField() ?>
