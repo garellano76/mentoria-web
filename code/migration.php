e4cce9b2f0b089862caef4aa42e4ce14d2b89336
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-06-16 18:38:51 -0400

    commit 11062021_09

---
+++ code/migration.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use app\core\Application;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DSN'],
+        'user' => $_ENV['USERNAME'],
+        'password' => $_ENV['PASSWORD'],
+    ]
+
+];
+
+$app = new Application(__DIR__, $config);
+
+$app->db->applyMigrations();
