422c043f82300b5ad4161b6f95447cd78cde4547
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-06-16 18:23:05 -0400

    commit 11062021_07

--- code/core/Database.php
+++ code/core/Database.php
@@ -8,9 +8,9 @@ class Database
 
     public function __construct(array $config)
     {
-        $dsn = $config['$dsn'] ?? '';
-        $username = $config['$user'] ?? '';
-        $password = $config['$password'] ?? '';
+        $dsn = $config['dsn'] ?? '';
+        $username = $config['user'] ?? '';
+        $password = $config['password'] ?? '';
 
         $this->pdo = new \PDO($dsn, $username, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
