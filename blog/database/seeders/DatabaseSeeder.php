2a587fc0a4bee7a1c5df661df112d045c34eda70
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-09-21 18:36:09 -0300

    Modificaciones clase_5

--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -17,32 +17,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::truncate();        
-        $user = User::factory()->create();
-
         Category::truncate();
+        Post::truncate();
 
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        $hobbies = Category::create([
-            'name' => 'Hobbies',
-            'slug' => 'hobbies'
-        ]);
-
-        Post::create([
-            'category_id' => $work->id,
-            'user_id' => $user->id,
-            'slug' => 'my-first-post',
-            'title' => 'my-first-post',
-            'resumen' => 'Resumen-post-1',
-            'body' => 'Texto-post-1'
-        ]);
+        Post::factory()->create();
+        
     }
 }
