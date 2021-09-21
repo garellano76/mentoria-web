5c29273bab556bccd412e20752bc24d12825c94f
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-09-21 18:39:28 -0300

    Modificaciones clase_6

--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
         Category::truncate();
         Post::truncate();
 
-        Post::factory()->create();
+        Post::factory(5)->create();
         
     }
 }
