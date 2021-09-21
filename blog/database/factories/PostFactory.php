100b948139585f34522d2ba3766b6d522839f9ec
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-09-21 18:07:06 -0300

    Modificaciones clase_2

--- blog/database/factories/PostFactory.php
+++ blog/database/factories/PostFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PostFactory extends Factory
