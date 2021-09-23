3064ee0ef529f3ce6757f340dc07553b888daa53
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-09-23 19:44:46 -0300

    Modificaciones clase_2

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -39,7 +39,7 @@ Route::get('/category/{category:slug}', function (Category $category) {
 
 Route::get('/author/{author}', function (User $author) {
     return view ('posts', [
-        'posts' => $author->posts,
+        'posts' => $author->posts->load(['category', 'author']),
     ]);     
 });
  
