1cc3ccb29c1a251c9c5f492e466573aff882ddc8
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-09-29 16:45:45 -0300

    Modificaciones clase_5

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -86,15 +86,12 @@
                                 <div class="space-x-2">
                                     <a href="#"
                                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                                       style="font-size: 10px">Techniques</a>
-                                    <a href="#"
-                                       class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
-                                       style="font-size: 10px">Updates</a>
+                                       style="font-size: 10px">{{$post->category->name}}</a>                                    
                                 </div>
 
                                 <div class="mt-4">
                                     <h1 class="text-3xl">
-                                        This is a big title and it will look great on two or even three lines. Wooohoo!
+                                        {{ $post->title }}
                                     </h1>
 
                                     <span class="mt-2 block text-gray-400 text-xs">
@@ -103,16 +100,14 @@
                                 </div>
                             </header>
 
+
+
                             <div class="text-sm mt-4">
                                 <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    {{ $post->resumen }}
                                 </p>
 
-                                <p class="mt-4">
-                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                </p>
+                                
                             </div>
 
                             <footer class="flex justify-between items-center mt-8">
@@ -125,7 +120,7 @@
                                 </div>
 
                                 <div>
-                                    <a href="#"
+                                    <a href="/post/{{$post->slug}}
                                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
                                     >Read More</a>
                                 </div>
