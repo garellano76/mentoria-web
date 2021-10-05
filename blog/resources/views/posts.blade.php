e03ec1ab8698a1a1f20027f09e8008859d196583
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-10-05 16:34:02 -0300

    Modificaciones clase_3

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -5,8 +5,8 @@
         <x-post-main :post="$posts[0]" />
 
         <div class="lg:grid lg:grid-cols-6">
-            @foreach ($posts->skip(1) as $post)
-                <x-post-card :post="$post" class="bg-red-500" />             
+            @foreach ($posts->skip(1) as $post)                
+                <x-post-card :post="$post" class="{{ $loop->iteration < 3 ? 'col-span->3' : 'col-span->2' }}" />             
             @endforeach
         </div>      
     </main>
