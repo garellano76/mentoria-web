047c006bd3bb942777ed6796e86ee6e6d6501362
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-10-07 18:39:10 -0300

    Modificaciones clase_6

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -21,7 +21,8 @@ Route::get('/', function () {
     return view('posts', [
         'posts' => Post::latest('published_at')
             ->with(['category', 'author'])
-            ->get()
+            ->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -33,13 +34,7 @@ Route::get('/post/{post}', function (Post $post) {
      
 Route::get('/category/{category:slug}', function (Category $category) {
     return view ('categories', [
-        //'posts' => $category->posts->load(['category', 'author']),        
-        'posts' => Post::join('categories', 'categories.id', '=', 'posts.category_id')
-                    ->where('posts.category_id', $category->id)
-                    ->latest('published_at')
-                    ->with(['category', 'author'])
-                    ->get()
-    ]);
+        'posts' => $category->posts->load(['category', 'author']),            ]);
 });
 
 Route::get('/author/{author}', function (User $author) {
