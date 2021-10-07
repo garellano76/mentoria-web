0234fb698d36605a1a56b4dd14b5a5d6507e0888
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-10-07 18:59:57 -0300

    Modificaciones clase_11

--- blog/resources/views/_post-header.blade.php
+++ blog/resources/views/_post-header.blade.php
@@ -22,7 +22,6 @@
                 @foreach ($categories as $category)
                     <option value="{{ $category->slug }}">{{ $category->name }}</option>
                 @endforeach                
-                
             </select>
 
             <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22" height="22" viewBox="0 0 22 22">
