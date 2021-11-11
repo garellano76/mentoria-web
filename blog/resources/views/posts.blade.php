da712c78f2cf29855a91867064e710e514c78788
Gonzalo Arellano <gonzalo.arellano@segic.usach.cl>
2021-11-11 18:05:56 -0300

    Modificaciones clase_1

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -9,7 +9,10 @@
                 @foreach ($posts->skip(1) as $post)                
                     <x-post-card :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}" />             
                 @endforeach
-            </div>      
+            </div>  
+            
+            {{ $posts->links() }}
+
         @else
             <p class="text-center">No Post yet.</p>
         @endif
